cc8d1e495ca1cdb5e34a90c393789c87469ed8fa
Imsiaocong <wd1998313@gmail.com>
2016-08-25 14:13:15 -0400

    Added

---
+++ Spritekit_Exp1/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  Spritekit_Exp1
+//
+//  Created by 王笛 on 16/8/25.
+//  Copyright (c) 2016年 王笛iOS.Inc. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
